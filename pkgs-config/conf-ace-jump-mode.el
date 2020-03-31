f75d4e37a2009351110796504d7681c358e32c11
mel <chuenshengm@gmail.com>
2020-04-01 07:59:33 +0800

    fixed package refresh problem, add new problem in readme.org

--- pkgs-config/conf-ace-jump-mode.el
+++ pkgs-config/conf-ace-jump-mode.el
@@ -6,7 +6,7 @@
 
 (use-package ace-jump-mode
   :config
-  (global-set-key (kbd "C-c .") 'ace-jump-mode) ;; globally bind "C-c ." with ace-jump-mode
+  (global-set-key (kbd "C-c j") 'ace-jump-mode) ;; globally bind "C-c ." with ace-jump-mode
   (autoload 'ace-jump-mode-pop-mark "ace-jump-mode" t))
 
 (provide 'conf-ace-jump-mode)
--- pkgs-config/conf-packages.el
+++ pkgs-config/conf-packages.el
@@ -15,18 +15,22 @@
 (setq package-archives '(("gnu" . "http://mirrors.163.com/elpa/gnu/")
 						 ("melpa" . "http://mirrors.163.com/elpa/melpa-stable/")))
 
-;; install packages if not installed
-(defun require-package (pkg)
-  "Install given PACKAGE"
-  (condition-case err
-	  (or (package-installed-p pkg)
-		  (package-install pkg))
-	(error (message "Couldn't install package: `%s': %S" pkg err) nil)))
-
 ;; do not activate installed packages
 (setq package-enable-at-startup nil)
 
 ;; initialize packages
 (package-initialize)
+
+;; refresh package list
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; install packages if not installed
+(defun require-package (pkg)
+  "Install given PACKAGE"
+  (condition-case err
+	  (unless (package-installed-p pkg)
+		(package-install pkg))
+	(error (message "Couldn't install package: `%s': %S" pkg err) nil)))
 
 (provide 'conf-packages)
