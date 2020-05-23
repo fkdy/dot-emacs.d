a2defca309ed4cb7c0ae3b959c806f12c5fec645
mel <chuenshengm@gmail.com>
2020-05-23 22:25:25 +0800

    fixed unsigned error for undo-tree

--- pkgs-config/conf-packages.el
+++ pkgs-config/conf-packages.el
@@ -9,7 +9,10 @@
 	   (expand-file-name
 		(format "pkgs/elpa-%s.%s" emacs-major-version emacs-minor-version)
 		user-emacs-directory)))
-  (setq package-user-dir versioned-pkg-dirs))
+  ;; set up package-user-dir
+  (setq package-user-dir versioned-pkg-dirs)
+  ;; set up package-gnupghome-dir for elpa gnupg
+  (setq package-gnupghome-dir (expand-file-name "gnupg" versioned-pkg-dirs)))
 
 ;; package repositories
 (setq package-archives '(("gnu" . "http://mirrors.163.com/elpa/gnu/")
@@ -21,10 +24,6 @@
 ;; initialize packages
 (package-initialize)
 
-;; refresh package list
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
 ;; install packages if not installed
 (defun require-package (pkg)
   "Install given PACKAGE"
@@ -32,5 +31,20 @@
 	  (unless (package-installed-p pkg)
 		(package-install pkg))
 	(error (message "Couldn't install package: `%s': %S" pkg err) nil)))
+
+;; refresh package list
+;; unconditionally update keyring for all versions
+(when (not package-archive-contents)
+  ;; disable signature checking
+  (setq package-check-signature nil)
+  ;; install gnu-elpa-keyring-update
+  (package-refresh-contents)
+  (require-package 'gnu-elpa-keyring-update)
+  ;; set signature checking to default value
+  (setq package-check-signature 'allow-unsigned)
+  ;; import keyring to versioned-pkg-dirs/gnupg
+  (package-import-keyring (expand-file-name "package-keyring.gpg" data-directory))
+  ;; update keyring
+  (gnu-elpa-keyring-update))
 
 (provide 'conf-packages)
