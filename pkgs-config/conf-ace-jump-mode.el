17b1a26ed6e09a856b994b0f2f90c261e8172aff
mel <chuenshengm@gmail.com>
2020-06-05 07:51:01 +0800

    use lower case char when searching

--- pkgs-config/conf-ace-jump-mode.el
+++ pkgs-config/conf-ace-jump-mode.el
@@ -7,6 +7,7 @@
 (use-package ace-jump-mode
   :config
   (global-set-key (kbd "C-c j") 'ace-jump-mode) ;; globally bind "C-c ." with ace-jump-mode
+  (setq ace-jump-mode-move-keys (loop for i from ?a to ?z collect i)) ;; use lower case char
   (autoload 'ace-jump-mode-pop-mark "ace-jump-mode" t))
 
 (provide 'conf-ace-jump-mode)
