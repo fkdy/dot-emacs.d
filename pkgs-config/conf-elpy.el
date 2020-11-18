ecf1ed03b1e28547bab9ab16b989ab4f280d64e0
meichuensheng <meichuensheng@meichuenshengdeMac-mini.local>
2020-11-18 18:50:10 +0800

    update python config in conf-elpy, conf-ggtags

--- pkgs-config/conf-elpy.el
+++ pkgs-config/conf-elpy.el
@@ -10,6 +10,11 @@
   :init
   (elpy-enable)
   :config
+  (setq elpy-rpc-virtualenv-path (mel/expand-auto-dir "elpy"))
+  (unless (getenv "WORKON_HOME")
+    (setenv "WORKON_HOME" elpy-rpc-virtualenv-path)
+    (mel/mkdir elpy-rpc-virtualenv-path))
+  (mel/mkdir elpy-rpc-virtualenv-path)
   (setq python-shell-interpreter "ipython")
   (setq elpy-rpc-python-command "python"))
 
--- pkgs-config/conf-ggtags.el
+++ pkgs-config/conf-ggtags.el
@@ -5,7 +5,7 @@
 (require-package 'ggtags)
 
 (use-package ggtags-mode
-  :hook (c-mode c++-mode python-mode verilog-mode)
+  :hook (c-mode c++-mode verilog-mode)
   :config (ggtags-mode 1))
 
 (provide 'conf-ggtags)
