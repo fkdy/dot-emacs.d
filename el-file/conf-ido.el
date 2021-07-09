f715409cba0461936fea7aecf1db52f23d304855
mel <chuenshengm@gmail.com>
2021-07-09 16:09:48 +0800

    add on-going config

--- el-file/conf-ido.el
+++ el-file/conf-ido.el
@@ -5,7 +5,6 @@
 (require 'ido)
 
 (use-package ido
-  :ensure t
   :init
   (setq ido-save-directory-list-file (mel/expand-auto-file "ido.last")
         ;; show any name that has the chars you typed
@@ -15,10 +14,9 @@
         confirm-nonexistent-file-or-buffer t
         ;; use current pane for newly opened file
         ido-default-file-method 'selected-window
+        ido-default-buffer-method 'selected-window
         ido-everywhere t)
   :config
-  ;; stop ido from suggesting when naming new file
-  (define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
   (add-hook 'after-init-hook 'ido-mode))
 
 (provide 'conf-ido)
