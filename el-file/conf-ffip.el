da9282798bfd0a768965dafd710c22600947c313
mel <chuenshengm@gmail.com>
2021-07-23 15:08:44 +0800

    set user-emacs-directory as ffip project root

--- el-file/conf-ffip.el
+++ el-file/conf-ffip.el
@@ -2,15 +2,15 @@
 ;; configure find-file-in-project
 ;;
 
-(require 'find-file-in-project)
+(mel/require-package 'find-file-in-project)
 
 (use-package find-file-in-project
   :config
   (if (eq system-type 'windows-nt)
       ;; use rust fd instead of GNU find for windows
-      (setq ffip-use-rust-fd t
-            ;; set emacs user directory as project root
-            ffip-project-root "~/.emacs.d")
+      (setq ffip-use-rust-fd t)
+    ;; set emacs-user-directory as project root
+    (setq ffip-project-root user-emacs-directory)
     ;; exclude hiden file/dir for GNU find
     (setq ffip-find-options "-not -path \"*/\.*\""))
   ;; bind key
