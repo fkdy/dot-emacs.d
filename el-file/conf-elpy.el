d9777f9d3a9702a2ae96e63877b2db1bcdbb7779
mel <chuenshengm@gmail.com>
2022-09-02 21:08:09 +0800

    [work] fixed elpy conf

--- el-file/conf-elpy.el
+++ el-file/conf-elpy.el
@@ -7,16 +7,14 @@
 (use-package elpy
   :defer t
   :init
-  (elpy-enable)
+  (with-eval-after-load 'python (elpy-enable))
   :config
-  (progn
-    (unless (getenv "WORKON_HOME")
-      (setenv "WORKON_HOME" (mel/expand-auto-dir "elpy"))
-      (mel/mkdir (getenv "WORKON_HOME")))
-    (setq elpy-rpc-virtualenv-path
-          (expand-file-name "emacs" (getenv "WORKON_HOME")))
-    (mel/mkdir elpy-rpc-virtualenv-path))
-  (setq python-shell-interpreter "ipython")
+  (setq elpy-rpc-virtualenv-path
+        (expand-file-name "emacs" (mel/expand-auto-dir "elpy")))
+  ;; set $WORKON_HOME env variable to the directory containing all the
+  ;; virtual env, so pyvenv-workon can select which virutal env to use.
+  (setenv "WORKON_HOME" (file-name-directory elpy-rpc-virtualenv-path))
+  ;; (setq python-shell-interpreter "ipython")
   (setq elpy-rpc-python-command "python"))
 
 (provide 'conf-elpy)
