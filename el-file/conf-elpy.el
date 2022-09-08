5cc78a3424e0e2784d693fa12c422490a677656f
mel <chuenshengm@gmail.com>
2022-09-08 15:42:48 +0800

    [work] fixed conf-elpy. loading default virtualenv after loading python.el

--- el-file/conf-elpy.el
+++ el-file/conf-elpy.el
@@ -7,14 +7,19 @@
 (use-package elpy
   :defer t
   :init
-  (with-eval-after-load 'python (elpy-enable))
+  (with-eval-after-load 'python
+    (elpy-enable)
+    (when (file-directory-p elpy-rpc-virtualenv-path)
+      ;; the 'emacs' virtualenv is the default virtualenv(assume the
+      ;; 'emacs' virutalenv has been installed in
+      ;; `(mel/expand-auto-dif "elpy/emacs")'
+      (pyvenv-workon "emacs")))
   :config
   (setq elpy-rpc-virtualenv-path
         (expand-file-name "emacs" (mel/expand-auto-dir "elpy")))
   ;; set $WORKON_HOME env variable to the directory containing all the
   ;; virtual env, so pyvenv-workon can select which virutal env to use.
   (setenv "WORKON_HOME" (file-name-directory elpy-rpc-virtualenv-path))
-  ;; (setq python-shell-interpreter "ipython")
   (setq elpy-rpc-python-command "python"))
 
 (provide 'conf-elpy)
