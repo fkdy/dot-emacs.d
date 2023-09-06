dc5f8e2676943ed95520c7cbd5e68e09b7167c34
mel <chuenshengm@gmail.com>
2023-09-06 18:57:40 +0800

    [work] set pyvenv-workon after loading elpy

--- el-file/conf-elpy.el
+++ el-file/conf-elpy.el
@@ -8,18 +8,18 @@
   :defer t
   :init
   (with-eval-after-load 'python
-    (elpy-enable)
-    (when (file-directory-p elpy-rpc-virtualenv-path)
-      ;; the 'emacs' virtualenv is the default virtualenv(assume the
-      ;; 'emacs' virutalenv has been installed in
-      ;; `(mel/expand-auto-dif "elpy/emacs")'
-      (pyvenv-workon "emacs")))
+    (elpy-enable))
   :config
   (setq elpy-rpc-virtualenv-path
         (expand-file-name "emacs" (mel/expand-auto-dir "elpy")))
   ;; set $WORKON_HOME env variable to the directory containing all the
   ;; virtual env, so pyvenv-workon can select which virutal env to use.
   (setenv "WORKON_HOME" (file-name-directory elpy-rpc-virtualenv-path))
-  (setq elpy-rpc-python-command "python"))
+  (setq elpy-rpc-python-command "python")
+  (when (file-directory-p elpy-rpc-virtualenv-path)
+    ;; the 'emacs' virtualenv is the default virtualenv(assume the
+    ;; 'emacs' virutalenv has been installed in
+    ;; `(mel/expand-auto-dir "elpy/emacs")'
+    (pyvenv-workon "emacs")))
 
 (provide 'conf-elpy)
